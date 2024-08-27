e63f73055a32c4774da0cdefbf97ea74137c07cb
Dominik Starke <5812061+DominikStarke@users.noreply.github.com>
2024-08-27 16:55:45 +0200

    General Android Support (#17)
    * Android 34 wants FLAG_IMMUTABLE
    
    * Gradle - Deprecated imperative apply
    
    * Broadcast Receiver is now explicit.
    
    * Lower reliance on magic strings
    
    * Update usb-serial-for-android to 3.8.0
    
    * Update Example with USB_DEVICE_ATTACHED intent
    
    * Java example on ACTION_USB_DEVICE_ATTACHED consumption
    
    ---------
    
    Co-authored-by: Dominik Starke <dominik.starke@becker-antriebe.com>

--- android/src/main/java/id/farellsujanto/flutter_serial_communication/FlutterSerialCommunicationPlugin.java
+++ android/src/main/java/id/farellsujanto/flutter_serial_communication/FlutterSerialCommunicationPlugin.java
@@ -191,6 +191,16 @@ public class FlutterSerialCommunicationPlugin implements FlutterPlugin, MethodCa
   public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
     activity = (FlutterActivity) binding.getActivity();
     usbManager = (UsbManager) activity.getSystemService(Context.USB_SERVICE);
+    // TODO: Application cold started by an ACTION_USB_DEVICE_ATTACHED intent - propagate to Flutter?
+    // TODO: For example connect to the device:
+    // Intent intent = activity.getIntent();
+    // String action = intent.getAction();
+    // if(action.equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
+    //   UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+    //   Log.i("Action", action);
+    //   Log.i("UsbDevice", device.getDeviceName());
+    //   connect(device.getDeviceName());
+    // }
   }
 
   @Override
@@ -275,7 +285,7 @@ public class FlutterSerialCommunicationPlugin implements FlutterPlugin, MethodCa
         activity.registerReceiver(
           usbGrantReceiver,
           new IntentFilter(PluginConfig.INTENT_ACTION_GRANT_USB),
-          Context.RECEIVER_EXPORTED
+          Context.RECEIVER_NOT_EXPORTED // this is correct, but android studio seems to have issues recognizing.
         );
       } else {
         activity.registerReceiver(
@@ -285,8 +295,9 @@ public class FlutterSerialCommunicationPlugin implements FlutterPlugin, MethodCa
       }
 
       PendingIntent usbGrantIntent = PendingIntent.getBroadcast(activity,
-          0,
-          new Intent(PluginConfig.INTENT_ACTION_GRANT_USB), flags);
+        0,
+        new Intent(PluginConfig.INTENT_ACTION_GRANT_USB).setPackage(activity.getPackageName()),
+        flags);
 
       usbManager.requestPermission(driver.getDevice(), usbGrantIntent);
     } else {
